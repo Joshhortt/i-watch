41366667f8a1155d0d11ff27a9a53c571a2853a3
Jose Horta <joshhortt@yahoo.com>
2019-04-03 20:04:18 +0100

    add minsHand' in script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 
 const secondHand = document.querySelector('.second-hand');
+const minsHand = document.querySelector('.min-hand');
 
 function setDate() {
     const now = new Date();
@@ -7,5 +8,9 @@ function setDate() {
     const seconds = now.getSeconds();
     const secondsDegrees = ((seconds / 60) * 360) + 90;
     secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+
+    const mins = now.getMinutes();
+    const minsDegrees = ((mins / 60) * 360) + ((seconds/60)*6) + 90;
+    minsHand.style.transform = `rotate(${minsDegrees}deg)`;
   }
   setInterval(setDate, 1000);

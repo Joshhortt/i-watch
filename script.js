423965968a042828e05fe8c84fa9df5d7ab73639
Jose Horta <joshhortt@yahoo.com>
2019-04-03 20:08:19 +0100

    add hourHand' in script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 
 const secondHand = document.querySelector('.second-hand');
 const minsHand = document.querySelector('.min-hand');
+const hourHand = document.querySelector('.hour-hand');
 
 function setDate() {
     const now = new Date();
@@ -12,5 +13,11 @@ function setDate() {
     const mins = now.getMinutes();
     const minsDegrees = ((mins / 60) * 360) + ((seconds/60)*6) + 90;
     minsHand.style.transform = `rotate(${minsDegrees}deg)`;
+
+    const hour = now.getHours();
+    const hourDegrees = ((hour / 60) * 360) + ((mins/60)*6) + 90;
+    hourHand.style.transform = `rotate(${hourDegrees}deg)`;
+
+
   }
   setInterval(setDate, 1000);
